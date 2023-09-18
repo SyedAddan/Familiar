9c6f32ac628a714a6535895a17bca09f0876df69
Syed Addan <syedaddan@gmail.com>
2023-09-18 10:46:35 +0500

    New Changes

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,9 +1,13 @@
 import './Home.css';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
       <h1>Hi Man</h1>
+      <button>
+        <Link to="/chatbot">Chat with me!</Link>
+      </button>
     </div>
   );
 }
--- client/src/pages/Routes.jsx
+++ client/src/pages/Routes.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-// import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import ChatBot from './ChatBot/ChatBot'
 import Home from './Home/Home'
 
 const App = () => {
     return (
-        <Home />
-        // <BrowserRouter>
-        //     <Routes>
-        //         <Route path='/' element={Home} />
-        //     </Routes>
-        // </BrowserRouter>
+        <BrowserRouter>
+            {/* <Home /> */}
+            <Routes>
+                <Route path='/' element={Home} />
+                <Route path='/chatbot' element={ChatBot} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
